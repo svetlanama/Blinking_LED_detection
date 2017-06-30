79d3909bfc9f2ed4aa99919526e9fd80f8c5b20c
Svitlana Moiseyenko <alexandrovna.sveta@gmail.com>
2017-06-30 03:44:07 +0300

    Intermediate result

--- Blinking_LED_detection/Blinking_LED_detection/ViewController.swift
+++ Blinking_LED_detection/Blinking_LED_detection/ViewController.swift
@@ -20,30 +20,9 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     @IBOutlet private weak var binCameraView: UIView!
     private let context = CIContext()
     private var count = 0
-    var visionRequests = [VNRequest]()
+   // var visionRequests = [VNRequest]()
     
     @IBOutlet weak var frameImageView: UIImageView!
-    
-    let filter = CIFilter(name: "CIColorControls", withInputParameters: nil)! //CIColorMap CISepiaTone CIColorControls
-    let blurfilter = CIFilter(name: "CIGaussianBlur")!
-    
-    /*func captured(ciImage: CIImage) {
-     //let orientation = CGImagePropertyOrientation(rawValue: UInt32(UIImageOrientation.up.rawValue))
-     //let imageElement = ciImage.applyingOrientation(Int32(orientation!.rawValue))
-     
-     filter.setValue(ciImage, forKey: kCIInputImageKey)
-     filter.setValue(-1.53, forKey: kCIInputBrightnessKey) //-1.53
-     filter.setValue(0.0, forKey: kCIInputSaturationKey)
-     filter.setValue(2.1, forKey: kCIInputContrastKey) //3
-     
-     if let output = filter.outputImage {
-     if let cgimg = context.createCGImage(output, from: output.extent) {
-     let processedImage = UIImage(cgImage: cgimg)
-     frameImageView.image = removeRotationForImage(image: processedImage)
-     //frameImageView.image?.set.imageOrientation = UIImageOrientation.Up
-     }
-     }
-     }*/
     
     // MARK: Sample buffer to UIImage conversion
     private func imageFromSampleBuffer(sampleBuffer: CMSampleBuffer) -> CIImage? {
@@ -66,6 +45,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         return session
     }()
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -79,6 +59,19 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         
         // begin the session
         self.captureSession.startRunning()
+        
+        
+        // set up the vision model
+//        guard let visionModel = try? VNCoreMLModel(for: Inceptionv3().model) else {
+//            fatalError("Could not load model")
+//        }
+//        // set up the request using our vision model
+//        let classificationRequest = VNCoreMLRequest(model: visionModel, completionHandler: handleClassifications)
+//        classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOptionCenterCrop
+//        visionRequests = [classificationRequest]
+        
+        //test(image: UIImage(named: "IMG_2259.PNG")!)
+       // searchRectangle(ciImage: CIImage(image: UIImage(named: "black14.jpg")!)!)
     }
     
     override func viewDidLayoutSubviews() {
@@ -91,22 +84,21 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     }()
     
     func handleRectangles(request: VNRequest, error: Error?) {
-        guard let observations = request.results as? [VNRectangleObservation]
+        guard let observations = request.results as? [VNDetectedObjectObservation]
             else {
-                print("unexpected result type from VNDetectRectanglesRequest")
+                print("unexpected result type from VNDetectedObjectObservation")
                 return
-                //fatalError("unexpected result type from VNDetectRectanglesRequest")
-                //
         }
-        guard let detectedRectangle = observations.first else {
+        guard let detectedObject = observations.first else {
            // DispatchQueue.main.async {
-                print("not detected rect")
+                print("not detected object")
             //}
             return
         }
         
-          print("=== detected rect ===")
+        print("=== detected object ===", detectedObject)
     }
+    
     
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
@@ -118,54 +110,47 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
                 kCIInputContrastKey: 2.5,
                 kCIInputBrightnessKey: -1.53
                 ])
-        
-        test(image: UIImage(ciImage: uiImage))
+         searchRectangle(ciImage: correctedImage)
+       // test(image: UIImage(named: "IMG_2259.PNG")!)
         DispatchQueue.main.async { [weak self] in //unowned
             
-            //.applyingFilter("CIColorInvert", withInputParameters: nil)
-            self?.frameImageView.image = UIImage(ciImage: correctedImage)
-            
-            
-//            var requestOptions: [VNImageOption: Any] = [:]
-//            let handler = VNImageRequestHandler(ciImage: correctedImage, options: requestOptions)
-//            DispatchQueue.global(qos: .userInteractive).async {
-//                do {
-//                    try handler.perform([self.rectanglesRequest])
-//                } catch {
-//                    print(error)
-//                }
-//            }
-            //self.captured(ciImage: uiImage)
+        //.applyingFilter("CIColorInvert", withInputParameters: nil)
+           self?.frameImageView.image = UIImage(ciImage: correctedImage)
         }
     }
     
-    func test(image: UIImage) -> Bool {
-       // var result  = 0
-       // var i = 0
-        let color = image.pixelColor(atLocation: CGPoint(x: 15, y: 15))
-            print("color:", color)
-            
-        
-        for  y in 0..<image.size.height.toInt() {
-            for x in 0..<image.size.width.toInt() {
-                
-//                let color = self.color   [self colorAt:image atX:x andY:y]
-//
-//                const CGFloat * colors = CGColorGetComponents(color.CGColor)
-//                let r = colors[0]
-//                let g = colors[1]
-//                let b = colors[2]
-//                result += .299 * r + 0.587 * g + 0.114 * b
-//                i++
+    func searchRectangle(ciImage: CIImage) {
+        var requestOptions: [VNImageOption: Any] = [:]
+        let handler = VNImageRequestHandler(ciImage: ciImage, options: requestOptions)
+        DispatchQueue.global(qos: .userInteractive).async {
+            do {
+                try handler.perform([self.rectanglesRequest])//[self.rectanglesRequest]) self.visionRequests
+            } catch {
+                print(error)
             }
         }
-        //float brightness = result / (float)i;
-        // NSLog(@"Image Brightness : %f",brightness);
-//        if (brightness > 0.8 || brightness < 0.3) {
-//            return false
-//        }
-        return true
     }
+    
+    
+    /*func handleClassifications(request: VNRequest, error: Error?) {
+        if let theError = error {
+            print("Error: \(theError.localizedDescription)")
+            return
+        }
+        guard let observations = request.results else {
+            print("No results")
+            return
+        }
+        let classifications = observations[0...4] // top 4 results
+            .flatMap({ $0 as? VNClassificationObservation })
+            .map({ "\($0.identifier) \(($0.confidence * 100.0).rounded())" })
+            .joined(separator: "\n")
+        
+        print("Object detected:", classifications)
+//        DispatchQueue.main.async {
+//            self.resultView.text = classifications
+//        }
+    }*/
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -181,7 +166,6 @@ extension UIImage {
         let data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
         
         let bytesPerPixel = cgImage.bitsPerPixel / 8
-        
         let pixelInfo: Int = ((cgImage.bytesPerRow * Int(point.y)) + (Int(point.x) * bytesPerPixel))
         
         let b = CGFloat(data[pixelInfo]) / CGFloat(255.0)
@@ -191,8 +175,6 @@ extension UIImage {
         
         return UIColor(red: r, green: g, blue: b, alpha: a)
     }
-    
-    
 }
 
 extension CGFloat {
@@ -200,3 +182,89 @@ extension CGFloat {
         return Int(self)
     }
 }
+
+
+
+/* func createHistorgamm(inputImage: UIImage) {
+ 
+ // let primaryView = GPUImageView(frame: binCameraView.frame)
+ //  view = primaryView
+ 
+ var sourcePicture = GPUImagePicture(image: inputImage, smoothlyScaleOutput: true)
+ 
+ // Creating another view to show histogram
+ let histView = GPUImageView(frame: CGRect(x: 0, y: 0,width: 200, height: 200))
+ histView.backgroundColor = UIColor.red
+ binCameraView.addSubview(histView)
+ binCameraView.bringSubview(toFront: histView)
+ 
+ // Create histogram filter and generator and point it to histogram view
+ let histFilter = GPUImageHistogramFilter()
+ let histGenerator = GPUImageHistogramGenerator()
+ histGenerator.forceProcessing(at: CGSizeMake(200.0, 200.0))
+ histGenerator.addTarget(histFilter)
+ 
+ 
+ histFilter.addTarget(histGenerator)
+ // Note target - hist view
+ histGenerator.addTarget(histView)
+ 
+ // Setup sepia filter just to show main picture
+ let sepiaFilter = GPUImageSepiaFilter()
+ sepiaFilter.forceProcessing(at: primaryView.sizeInPixels)
+ 
+ sourcePicture?.addTarget(sepiaFilter)
+ // Note target - main view
+ sepiaFilter.addTarget(primaryView)
+ 
+ sourcePicture?.processImage()
+ 
+ }
+
+func createHistorgamm(inputImage: UIImage) {
+    let filter = GPUImageHistogramFilter(HistogramType.luminance)
+    
+    let histogramGraph = GPUImageHistogramGenerator()
+    histogramGraph.forceProcessingAtSize(CGSize(200.0, 200.0))
+    filter.addTaget(histogramGraph)
+    
+    let blendFilter =  GPUImageAlphaBlendFilter()
+    blendFilter.mix = 0.75
+    blendFilter.forceProcessingAtSize(CGSize(200.0, 200.0))
+    cameraView.addTarget(blendFilter)
+    histogramGraph.addTarget(blendFilter)
+    blendFilter.addTagret(filterView)
+    
+}
+ 
+ 
+ 
+ func test(image: UIImage) -> Bool {
+ // var result  = 0
+ // var i = 0
+ 
+ 
+ 
+ for  y in 0..<image.size.height.toInt() {
+ print("===height===")
+ for x in 0..<image.size.width.toInt() {
+ let color = image.pixelColor(atLocation: CGPoint(x: x, y: y))
+ print("color:", color)
+ //                let color = self.color   [self colorAt:image atX:x andY:y]
+ //
+ //                const CGFloat * colors = CGColorGetComponents(color.CGColor)
+ //                let r = colors[0]
+ //                let g = colors[1]
+ //                let b = colors[2]
+ //                result += .299 * r + 0.587 * g + 0.114 * b
+ //                i++
+ }
+ }
+ //float brightness = result / (float)i;
+ // NSLog(@"Image Brightness : %f",brightness);
+ //        if (brightness > 0.8 || brightness < 0.3) {
+ //            return false
+ //        }
+ return true
+ }
+ */
